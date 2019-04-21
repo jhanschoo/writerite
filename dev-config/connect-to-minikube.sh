1bc7aad8a5d6f79016f63bfd7a0de9d8b6f41608
Johannes Choo <jhanschoo@gmail.com>
2019-04-21 11:23:45 +0800

    add fix to ignore known hosts on dev connection

--- dev-config/connect-to-minikube.sh
+++ dev-config/connect-to-minikube.sh
@@ -1,2 +1,2 @@
 ssh-keygen -f "/home/jhanschoo/.ssh/known_hosts" -R "$(minikube ip)"
-ssh -o "StrictHostKeyChecking no" -i "~/.minikube/machines/minikube/id_rsa" "docker@$(minikube ip)" -L "4000:127.0.0.1:443"
+ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -i "~/.minikube/machines/minikube/id_rsa" "docker@$(minikube ip)" -L "4000:127.0.0.1:443"
