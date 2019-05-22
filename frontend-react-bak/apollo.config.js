d32f32d0f95b6c77c5e4d70c03caacc1ee33d2e4
Johannes Choo <jhanschoo@gmail.com>
2019-05-22 10:07:45 +0800

    delete obsolete projects

--- frontend-react-bak/apollo.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  client: {
-    service: {
-      name: 'writerite-backend-yoga',
-      localSchemaFile: '../backend-yoga/src/schema/schema.graphql'
-    }
-  }
-};
--- frontend-react-bak/src/modules/card/types.ts
+++
@@ -1,9 +0,0 @@
-import { Payload } from '../types';
-
-export interface WrCard {
-  readonly id: string;
-  readonly front: string;
-  readonly back: string;
-}
-
-export type CardUpdatesPayload = Payload<WrCard>;
