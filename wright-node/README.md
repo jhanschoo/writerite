2e6c582280e18bb192698ff4a3a007833dc8ed1b
Johannes Choo <jhanschoo@gmail.com>
2019-07-16 13:16:36 +0800

    add correct quizServer and documentation

--- wright-node/README.md
+++ wright-node/README.md
@@ -13,6 +13,41 @@ Worker servicing chat in `@writerite/backend-yoga` as a chatroom bot. Check `kub
   * Containerization: Docker
   * Orchestration: Kubernetes
 
-## TODO
+## Config Communication
 
-* communication API needs huge refactor
+* Config messages specify where to place the config message in chat.
+  they do not contain anything about the current config state.
+
+## Quiz Round Framework API Sketch
+
+* `quizServer(channel: string, rounds: Round[]): Promise<void>` asynchronously serves a quiz consisting of rounds.
+  * `channel` is the channel on which to listen for messages.
+  * `rounds` is an array of `Round`s that may be mutated by `quizServer`.
+  * Each round handler `Round` is a function (thunk) `() => Promise<INextParams>`.
+  * `INextParams` is an object with the following properties:
+    * `rounds?: Round[]`,
+    * `delay?: number | null`,
+    * `messageHandler?: MessageHandler`, where
+      * Each `MessageHandler` is a function (thunk) `(message: string) => Promise<INextParams>`.
+
+### Behavior
+
+When invoked, `quizServer` invokes the rounds handlers in `rounds` and message handlers with a guarantee that only one invocation among the round handlers and message handlers is active at a time, as long as every round handler and message handler have no deferred execution left in the event loop when they resolve.
+
+Whenever `quizServer` tries to invoke a round handler, it does so by trying to `pop` the last round handler off the `rounds` array. If the `rounds` array is empty at that point, it resolves.
+
+The return object `INextParams` tells `quizServer` how to serve the next round, and what to do until it serves the next round.
+
+* `rounds`, if defined, replaces the current `rounds` array with this one. `quizServer` may mutate this array.
+* `delay`, if `undefined`, schedules the next round to run as soon as possible. If `null`, it does not schedule the next round. If an integer, it schedules the next round to run as soon as possible after `delay` milliseconds, but it cannot "push back" a previous scheduling. That is, if it has already been scheduled to run at an earlier future time, the next round will begin as soon as possible after that earlier time; on the other hand, if it has already been scheduled to run at a later future time, the schedule will be brought forward to this earlier future time.
+* `messageHandler`, if defined, and if delay is also defined, specifies how to handle messages until the next round begins. If either `messageHandler` or `delay` are left undefined, the old behavior is retained. If messages are received after a new round begins but a new `messageHandler` has not (yet) been defined, messages received will not be handled by an old `messageHandler` but silently dropped.
+
+### Some Guarantees and Limitations
+
+* Every round handler and message handler must have no deferred execution left in the event loop when they resolve. They should eventually resolve or reject.
+* Until an invocation of `quizServer` resolves, every `rounds` passed in the initial invocation and via `INextParams` should not be mutated outside the round handlers and message handlers passed to that invocation. Conversely, it is allowed to mutate `rounds` from within the round handlers and message handlers.
+* `quizServer` guarantees that only one invocation among the round handlers and message handlers (passed to that invocation) is active at a time. That is, another round handler or message handler will not be invoked wrt this invocation of `quizServer` while a round handler or message handler is yet to resolve.
+* `quizServer` immediately rejects and stops processing queued rounds and messages upon encountering an error.
+* `quizServer` guarantees that a message handler for a given round will not be invoked when a future round has already started.
+* `quizServer` guarantees in-order for message handling.
+* `quizServer` does not guarantee starvation-freedom for rounds under a high load of incoming messages.
---
+++ wright-node/src/quizServer.ts
@@ -0,0 +1,117 @@
+import { createClient } from './redisClient';
+
+export interface INextParams {
+  rounds?: Round[];
+  delay?: number | null;
+  messageHandler?: MessageHandler;
+}
+
+export type Round = () => Promise<INextParams>;
+
+export type MessageHandler = (message: string) => Promise<INextParams>;
+
+type WrappedMessageHandler = (message: string) => Promise<void>;
+
+const DO_NOTHING: WrappedMessageHandler = (message: string) => Promise.resolve();
+
+export const quizServer = (channel: string, rounds: Round[]) => {
+  if (rounds.length === 0) {
+    return Promise.resolve();
+  }
+  const client = createClient();
+  let roundNum = 0;
+  let safeMessageHandler = DO_NOTHING;
+  return new Promise<void>((res, rej) => {
+    const readyListener = () => {
+      client.subscribe(channel);
+      client.on('message', messageListener);
+      queue(thunkRound(0));
+    };
+    const messageListener = (_channel: string, message: string) => {
+      // safeMessageHandler is shared, so we retain a reference to
+      // the current handler upon invocation, so its identity does
+      // not change when it is finally invoked. Ultimately this
+      // means that some messages received before the next round
+      // begins are dropped rather than handled with the next
+      // round's message handler (good), though not all due to race
+      // conditions out of our control. Perhaps consider just naively
+      // using safeMessageHandler.
+      const currentMessageHandler = safeMessageHandler;
+      queue(() => currentMessageHandler(message));
+    };
+    const quitHandler = () => {
+      // eventually cleanup queue and client on error; client's
+      // event handler possesses last reference to queue.;
+      client.quit(() => {
+        client.off('ready', readyListener);
+        client.off('message', messageListener);
+      });
+    };
+    const queue = (() => {
+      let _queue = Promise.resolve();
+      return (p: () => Promise<void>) => {
+        _queue = _queue.then(p).catch((reason: any) => {
+          // all queued thunks get dropped when when rejected.
+          // eventually cleanup queue and client on fulfilment; client's
+          // event handler possesses last reference to queue.
+          // When done, queue should be available for GC.
+          quitHandler();
+          rej(reason);
+        });
+      };
+    })();
+    const thunkRound = (tag: number) => {
+      const nextTag = tag + 1;
+      const queueNextRound = (): void => queue(thunkRound(nextTag));
+      const handleNextRoundAndMessageHandler = ({ rounds: newRounds, delay, messageHandler }: INextParams): void => {
+        if (newRounds) {
+          rounds = newRounds;
+        }
+        if (delay === undefined) {
+          return queueNextRound();
+        }
+        if (delay !== null) {
+          setTimeout(queueNextRound, delay);
+        }
+        if (messageHandler) {
+          safeMessageHandler = tagMessageHandler(messageHandler);
+        }
+      };
+      const tagMessageHandler = (messageHandler: MessageHandler) => async (message: string) => {
+        // noop if surfaced in queue but next round has come/all rounds
+        // have ended
+        // nextTag is used since current round has already incremented
+        // roundNum
+        if (nextTag !== roundNum) {
+          return;
+        }
+        return handleNextRoundAndMessageHandler(await messageHandler(message));
+      };
+      return async () => {
+        // noop if surfaced in queue but next round has come/all rounds
+        // have ended
+        if (tag !== roundNum) {
+          return;
+        }
+        roundNum = nextTag;
+        const round = rounds.pop();
+        if (!round) {
+          // ignore all queued thunks, by our construction of thunks,
+          // with the below setting they fulfil without queueing yet
+          // more events.
+          roundNum = Infinity;
+          // eventually cleanup queue and client on fulfilment; client's
+          // event handler possesses last reference to queue.
+          quitHandler();
+          return res();
+          // once returned, queued thunks all get fulfilled and queue
+          // becomes available for GC since no more references or work
+          // left. Unsure if ioredis leaks memory even when cleaned
+          // up as per documentation.
+        }
+        return handleNextRoundAndMessageHandler(await round());
+      };
+    };
+    client.on('ready', readyListener);
+  });
+};
