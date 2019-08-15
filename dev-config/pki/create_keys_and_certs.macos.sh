8cfc029f046619a85088f7a5eef051700c6f958f
Johannes Choo <jhanschoo@gmail.com>
2019-08-15 17:29:40 +0800

    add script to create keys and certs for macos

--- dev-config/pki/create_keys_and_certs.macos.sh
+++ dev-config/pki/create_keys_and_certs.macos.sh
@@ -1,7 +1,7 @@
 # macOS keychain does not recognize EC-signed CA keys, it seems. Fallback to RSA for all
 
 # create wr-dev CA private key
-openssl genpkey -algorithm RSA -out wr-dev.secp256r1.ca.private -pkeyopt rsa_keygen_bits:2048
+openssl genpkey -algorithm RSA -out wr-dev.secp256r1.ca.private -pkeyopt rsa_keygen_bits:4096
 
 # create wr-dev CA csr
 openssl req -new -key wr-dev.secp256r1.ca.private -out wr-dev.secp256r1.ca.csr -config req.ca.config
@@ -10,7 +10,7 @@ openssl req -new -key wr-dev.secp256r1.ca.private -out wr-dev.secp256r1.ca.csr -
 openssl x509 -req -in wr-dev.secp256r1.ca.csr -signkey wr-dev.secp256r1.ca.private -out wr-dev.secp256r1.ca.crt
 
 # create wr-dev private key
-openssl genpkey -algorithm RSA -out wr-dev.secp256r1.private -pkeyopt rsa_keygen_bits:2048
+openssl genpkey -algorithm RSA -out wr-dev.secp256r1.private -pkeyopt rsa_keygen_bits:4096
 
 # create wr-dev csr
 openssl req -new -key wr-dev.secp256r1.private -out wr-dev.secp256r1.csr -config req.config
