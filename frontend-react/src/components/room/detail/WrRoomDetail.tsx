67644d0f5639833fa3aeea3d48663d7c9bd8d0d5
Johannes Choo <jhanschoo@gmail.com>
2020-08-08 19:26:44 +0800

    eslint ignore comment

--- frontend-react/src/components/room/detail/WrRoomDetail.tsx
+++ frontend-react/src/components/room/detail/WrRoomDetail.tsx
@@ -5,6 +5,7 @@ import { Main } from "src/ui";
 import WrRoomNavBar from "../navbar/WrRoomNavBar";
 
 const WrRoomDetail = (): JSX.Element => {
+  // eslint-disable-next-line no-shadow
   const history = useHistory<{ deckId: string } | null>();
   const { deckId } = history.location.state ?? {};
   return <>
