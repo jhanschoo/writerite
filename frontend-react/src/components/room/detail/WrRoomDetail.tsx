fa36ba44ea7126f880dddb4ca509483dfd0d29e7
Johannes Choo <jhanschoo@gmail.com>
2020-08-08 18:27:55 +0800

    add useHistory to WrRoomDetail (may be refactored)

--- frontend-react/src/components/room/detail/WrRoomDetail.tsx
+++ frontend-react/src/components/room/detail/WrRoomDetail.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { useHistory } from "react-router";
 
 import { Main } from "../../../ui";
 import WrRoomNavBar from "../navbar/WrRoomNavBar";
 
 const WrRoomDetail = (): JSX.Element => {
-  return (<>
+  const history = useHistory<{ deckId: string } | null>();
+  const { deckId } = history.location.state ?? {};
+  return <>
     <WrRoomNavBar />
     <Main />
-  </>);
+  </>;
 };
 
 export default WrRoomDetail;
