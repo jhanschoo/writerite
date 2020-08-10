7bdf58cc439244c81e5897db5c9d898a6f840582
Johannes Choo <jhanschoo@gmail.com>
2020-08-10 10:24:39 +0800

    comment on separation of gql's

--- frontend-react/src/gql/index.ts
+++ frontend-react/src/gql/index.ts
@@ -1,2 +1,15 @@
+/*
+ * Note: the gql's are separated by query/mutation/subscription
+ * for ease of writing update functions:
+ * In general, data from queries can become stale in important places
+ * that has a large negative hit on UX, whereas data directly from
+ * mutations and subscriptions are generally used in ways that are
+ * tolerated if they become stale.
+ * Hence the separation allows us to easily scan for queries that need
+ * updating while writing an update function for a mutation or
+ * subscription witnessing an entity being created or deleted.
+ */
+
+
 export * from "./queries";
 export * from "./mutations";
