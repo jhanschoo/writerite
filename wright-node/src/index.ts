b4c858d9bb3681459d2ab752cbe19bd9927e1155
Johannes Choo <jhanschoo@gmail.com>
2020-08-14 08:59:53 +0800

    add test for successful connection from wrightNode

--- wright-node/src/index.ts
+++ wright-node/src/index.ts
@@ -3,12 +3,14 @@ import { client } from "./apolloClient";
 
 import { RoomsUpdatesSubscription } from "./gql/gqlTypes/RoomsUpdatesSubscription";
 import { ROOMS_UPDATES_SUBSCRIPTION } from "./gql/subscription";
+import { USER_QUERY } from "./gql/queries";
+import { UserQuery, UserQueryVariables } from "./gql/gqlTypes/UserQuery";
 
-// void client.query<UserQuery, UserQueryVariables>({
-//   query: USER_QUERY,
-//   variables: { id: "bcedd80c-0f81-4fb0-a2b5-839820a23400" },
-// })
-//   .then((data) => console.log(data));
+void client.query<UserQuery, UserQueryVariables>({
+  query: USER_QUERY,
+  variables: { id: "a0236142-0cb2-4c70-b32a-3ccca8f625b0" },
+})
+  .then((data) => console.log(data));
 
 void client.subscribe<RoomsUpdatesSubscription>({
   query: ROOMS_UPDATES_SUBSCRIPTION,
