edfb9d2bc1f051ad8c4127e2622137fec766c342
Johannes Choo <jhanschoo@gmail.com>
2020-09-16 10:40:28 +0800

    remove hostPort annotations (on writerite-backend)

--- wright-node/dockerbuild.sh
+++ wright-node/dockerbuild.sh
@@ -15,7 +15,7 @@ PACKAGE_VERSION=$(node -pe "require('./package.json').version")
 SUFFIX="-$NODE_ENV"
 if [ "$SUFFIX" = "-development" ]
 then
-  SUFFIX="-dev.8"
+  SUFFIX="-dev.9"
 fi
 if [ "$SUFFIX" = "-production" ]
 then
