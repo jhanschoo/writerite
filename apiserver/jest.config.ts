f4b4d20efae481fb453bf76c9b7ef576bc6ade3c
Johannes Choo <jhanschoo@gmail.com>
2021-12-28 16:41:54 +0800

    update apiserver jest config

--- apiserver/jest.config.ts
+++ apiserver/jest.config.ts
@@ -191,7 +191,11 @@ export default {
 	// verbose: undefined,
 
 	// An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
-	// watchPathIgnorePatterns: [],
+	watchPathIgnorePatterns: [
+		"/node_modules/",
+		"/generated/",
+		"/coverage/"
+	],
 
 	// Whether to use watchman for file crawling
 	// watchman: true,
