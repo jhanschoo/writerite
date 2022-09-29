f341c4e18d3894f2643be5723ff7bcf798e518d1
Johannes Choo <jhanschoo@gmail.com>
2022-09-29 12:01:46 +0800

    cleanup
    Signed-off-by: Johannes Choo <jhanschoo@gmail.com>

--- frontend-next/src/features/manageDeck/components/ManageDeckCardsUploadImport.tsx
+++ frontend-next/src/features/manageDeck/components/ManageDeckCardsUploadImport.tsx
@@ -15,7 +15,6 @@ interface Props {
 
 export const ManageDeckCardsUploadImport: FC<Props> = ({ onPreviousStep, onSuccessfulImport }) => {
   const [loading, setLoading] = useState<boolean>(false);
-  const [cards, setCards] = useState<null | NewCardData[]>(null)
   const [hasErrors, setHasErrors] = useState<boolean>(false);
   const parseCsv = useParseCsv();
   const handleDrop = async (files: File[]) => {
@@ -34,64 +33,60 @@ export const ManageDeckCardsUploadImport: FC<Props> = ({ onPreviousStep, onSucce
       setLoading(false);
     }
   };
-  if (cards) {
-    return null;
-  } else {
-    return (
-      <Stack>
-        <Dropzone
-          onDrop={handleDrop}
-          onReject={(files) => console.log('rejected files', files)}
-          maxSize={MAX_SIZE_MIB * 1024 ** 2}
-          multiple={false}
-          loading={loading}
-          /*
-          * Note that there is a file chooser error (automatically cancels and exits)
-          * when accept is defined, for Chrome on GTK 4 (?), when useFsAccessApi={true}.
-          * 
-          * In more detail,
-          * the showOpenFilePicker method in the File System Access API available
-          * on Blink-based browsers allows for an optional `description` field
-          * when restricting accepted types, and the GTK file picker errors out
-          * when this field is undefined or an empty string (at least).
-          * Next, the underlying library `react-dropzone` on browsers supporting
-          * the File System Access API uses it unless a flag is set, and uses
-          * it in such a way that does not expose a way to set the `description`
-          * property. That is unless we have set `useFsAccessApi` to false to use
-          * another method of uploading files.
-          */
-          // A bug report has been submitted to the Chromium project:
-          // https://bugs.chromium.org/p/chromium/issues/detail?id=1350487
-          accept={{[MIME_TYPES.csv]: ['.csv']}}
-          sx={{
-            minHeight: '50vh',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            textAlign: 'center',
-          }}
-          useFsAccessApi={false}
-        >
-          <Stack spacing="md" align="center">
-            <Text size="lg">
-              Drag a .csv file here
+  return (
+    <Stack>
+      <Dropzone
+        onDrop={handleDrop}
+        onReject={(files) => console.log('rejected files', files)}
+        maxSize={MAX_SIZE_MIB * 1024 ** 2}
+        multiple={false}
+        loading={loading}
+        /*
+        * Note that there is a file chooser error (automatically cancels and exits)
+        * when accept is defined, for Chrome on GTK 4 (?), when useFsAccessApi={true}.
+        * 
+        * In more detail,
+        * the showOpenFilePicker method in the File System Access API available
+        * on Blink-based browsers allows for an optional `description` field
+        * when restricting accepted types, and the GTK file picker errors out
+        * when this field is undefined or an empty string (at least).
+        * Next, the underlying library `react-dropzone` on browsers supporting
+        * the File System Access API uses it unless a flag is set, and uses
+        * it in such a way that does not expose a way to set the `description`
+        * property. That is unless we have set `useFsAccessApi` to false to use
+        * another method of uploading files.
+        */
+        // A bug report has been submitted to the Chromium project:
+        // https://bugs.chromium.org/p/chromium/issues/detail?id=1350487
+        accept={{[MIME_TYPES.csv]: ['.csv']}}
+        sx={{
+          minHeight: '50vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          textAlign: 'center',
+        }}
+        useFsAccessApi={false}
+      >
+        <Stack spacing="md" align="center">
+          <Text size="lg">
+            Drag a .csv file here
+          </Text>
+          <Divider label="or" sx={{ alignSelf: "stretch" }} labelPosition="center" />
+          <Button>Select file</Button>
+          <Text color="dimmed" size="xs">
+            Maximum file size limit: {MAX_SIZE_MIB}MB
+          </Text>
+          {hasErrors && (
+            <Text color="red" size="xs">
+              There were errors importing the file. Please check and try again.
             </Text>
-            <Divider label="or" sx={{ alignSelf: "stretch" }} labelPosition="center" />
-            <Button>Select file</Button>
-            <Text color="dimmed" size="xs">
-              Maximum file size limit: {MAX_SIZE_MIB}MB
-            </Text>
-            {hasErrors && (
-              <Text color="red" size="xs">
-                There were errors importing the file. Please check and try again.
-              </Text>
-            )}
-          </Stack>
-        </Dropzone>
-        <Button sx={{ alignSelf: "flex-start" }} variant="subtle" onClick={onPreviousStep} leftIcon={<ArrowLeftIcon />}>
-          Review instructions
-        </Button>
-      </Stack>
-    );
-  }
+          )}
+        </Stack>
+      </Dropzone>
+      <Button sx={{ alignSelf: "flex-start" }} variant="subtle" onClick={onPreviousStep} leftIcon={<ArrowLeftIcon />}>
+        Review instructions
+      </Button>
+    </Stack>
+  );
 };
