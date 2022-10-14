e83c31a882befbbd329cec6b6efaa26416bdd1cd
Johannes Choo <jhanschoo@gmail.com>
2022-10-14 23:43:22 +0800

    Implement single card creation in deck
    Signed-off-by: Johannes Choo <jhanschoo@gmail.com>

--- frontend-next/src/features/manageDeck/components/ManageDeckCardsBrowse.tsx
+++ frontend-next/src/features/manageDeck/components/ManageDeckCardsBrowse.tsx
@@ -1,19 +1,28 @@
 import { ChangeEvent, FC, useMemo, useState } from 'react';
-import { Pagination, Stack, TextInput } from '@mantine/core';
+import { useMutation } from 'urql';
+import { Button, Pagination, Stack, TextInput } from '@mantine/core';
 
 import { ManageDeckProps } from '../types/ManageDeckProps';
 import { Delta } from 'quill';
 import { ManageCard } from '@/features/manageCard';
+import { CardCreateDocument } from '@generated/graphql';
 
 type Card = ManageDeckProps['deck']['cardsDirect'][number];
 
-const sortCards = (cards: Card[]) => {
-  cards.sort((a, b) => a.editedAt > b.editedAt ? 1 : a.editedAt < b.editedAt ? -1 : a.id > b.id ? 1 : -1);
-}
+const sortCards = (cards: Card[]) => cards.sort((a, b) => a.editedAt > b.editedAt ? -1 : a.editedAt < b.editedAt ? 1 : a.id > b.id ? 1 : -1);
 
 export const ManageDeckCardsBrowse: FC<ManageDeckProps> = ({ deck }) => {
   const [filter, setFilter] = useState('');
   const [activePage, setActivePage] = useState<number>(1);
+  const [{ fetching }, deckAddCards] = useMutation(CardCreateDocument);
+  const handleAddNewCard = () => deckAddCards({
+    deckId: deck.id,
+    card: {
+      prompt: {},
+      fullAnswer: {},
+      answers: [],
+    },
+  });
   const handleFilterChange = (e: ChangeEvent<HTMLInputElement>) => {
     setFilter(e.target.value);
     setActivePage(1);
@@ -28,10 +37,12 @@ export const ManageDeckCardsBrowse: FC<ManageDeckProps> = ({ deck }) => {
     return filteredCards.slice((activePage - 1) * 10, activePage * 10);
   }, [deck.cardsDirect, activePage, filter]);
   const total = Math.ceil(deck.cardsDirect.length / 10);
+  const canAddANewCard = deck.cardsDirect.length < parseInt(process.env.NEXT_PUBLIC_MAX_CARDS_PER_DECK as string) && activePage === 1;
   return (
     <Stack align="stretch">
       <TextInput value={filter} onChange={handleFilterChange} label="Search cards" sx={{ flexGrow: 1 }} />
       {total ? <Pagination page={activePage} onChange={setActivePage} total={total} radius="lg" sx={{ alignSelf: "center" }} /> : undefined}
+      {canAddANewCard && <Button onClick={handleAddNewCard}>New Card</Button>}
       {currentCards.map((card) => <ManageCard card={card} key={card.id} onDelete={() => undefined} forceLoading={false} />)}
       {total ? <Pagination page={activePage} onChange={setActivePage} total={total} radius="lg" sx={{ alignSelf: "center" }} /> : undefined}
     </Stack>
--- frontend-next/src/lib/urql/common.ts
+++ frontend-next/src/lib/urql/common.ts
@@ -2,14 +2,14 @@ import { SSRExchange } from 'next-urql';
 import { dedupExchange, fetchExchange, makeOperation, subscriptionExchange } from 'urql/core';
 import { devtoolsExchange } from '@urql/devtools';
 import { authExchange } from '@urql/exchange-auth';
-import { cacheExchange } from '@urql/exchange-graphcache';
+import { cacheExchange, Data, NullArray } from '@urql/exchange-graphcache';
 import { getAccessKey } from '@lib/tokenManagement';
 import { createClient } from 'graphql-ws';
 import WebSocket from 'isomorphic-ws';
 import schema from '@root/graphql.schema.json';
 import { IntrospectionData } from '@urql/exchange-graphcache/dist/types/ast';
 import { isSSRContext, NEXT_PUBLIC_GRAPHQL_HTTP, NEXT_PUBLIC_GRAPHQL_WS } from '@/utils';
-import { DeckCardsDirectCountFragmentDoc, DeckSummaryFragmentDoc, Mutation } from '@generated/graphql';
+import { DeckCardsDirectCountFragmentDoc, Mutation } from '@generated/graphql';
 
 export const commonUrqlOptions = {
   url: NEXT_PUBLIC_GRAPHQL_HTTP,
@@ -68,11 +68,22 @@ export const getExchanges = (ssr: SSRExchange) => [
     schema: schema as IntrospectionData,
     updates: {
       Mutation: {
+        cardCreate(result, _args, cache, _info) {
+          const { cardCreate: card } = result as Mutation;
+          const { id, deckId } = card;
+          const cardsDirect = cache.resolve({ __typename: 'Deck', id: deckId as string }, 'cardsDirect');
+          if (Array.isArray(cardsDirect)) {
+            cardsDirect.unshift({ __typename: 'Card', id });
+            cache.link({ __typename: 'Deck', id: deckId as string }, 'cardsDirect', cardsDirect as NullArray<Data>);
+            cache.writeFragment(DeckCardsDirectCountFragmentDoc, { id: deckId as string, cardsDirectCount: cardsDirect.length });
+          }
+        },
         cardDelete(result, _args, cache, _info) {
           const { cardDelete: { id, deckId } } = result as Mutation;
           const cardsDirect = cache.resolve({ __typename: 'Deck', id: deckId as string }, 'cardsDirect');
+          const deletedCardKey = cache.keyOfEntity({ __typename: 'Card', id })
           if (Array.isArray(cardsDirect)) {
-            const updatedCards = cardsDirect.filter((cardKey) => cardKey !== cache.keyOfEntity({ __typename: 'Card', id }));
+            const updatedCards = cardsDirect.filter((cardKey) => cardKey !== deletedCardKey);
             cache.link({ __typename: 'Deck', id: deckId as string }, 'cardsDirect', updatedCards);
             cache.writeFragment(DeckCardsDirectCountFragmentDoc, { id: deckId as string, cardsDirectCount: updatedCards.length });
           }
