56f9eaeab8fb5f30f6eb07974f6c0badc26ab27d
Johannes Choo <jhanschoo@gmail.com>
2022-10-16 20:03:54 +0800

    Limit RTE toolbar to supported
    Signed-off-by: Johannes Choo <jhanschoo@gmail.com>

--- frontend-next/src/components/RichTextEditor/index.tsx
+++ frontend-next/src/components/RichTextEditor/index.tsx
@@ -1,6 +1,15 @@
 import type { RichTextEditorProps } from '@mantine/rte';
+import type { ToolbarControl } from '@mantine/rte/lib/components/Toolbar/controls';
 import dynamic from 'next/dynamic';
 import type { Ref } from 'react';
+
+export const SUPPORTED_RTE_TOOLBAR: ToolbarControl[][] = [
+  ['bold', 'italic', 'underline', 'strike', 'link', 'clean'],
+  ['h1', 'h2', 'h3', 'h4'],
+  ['unorderedList', 'orderedList'],
+  ['alignLeft', 'alignCenter', 'alignRight'],
+  ['sup', 'sub']
+];
 
 export default dynamic(async () => {
   const { default: Editor } = await import('@mantine/rte');
@@ -8,7 +17,7 @@ export default dynamic(async () => {
     { forwardedRef, ...props }:
       RichTextEditorProps
       & { forwardedRef?: Ref<any> }
-  ) => <Editor ref={forwardedRef} {...props} />;
+  ) => <Editor ref={forwardedRef} controls={SUPPORTED_RTE_TOOLBAR} {...props} />;
 }, {
   // Disable during server side rendering
   ssr: false,
